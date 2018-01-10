40eb7699aa93395f13b1a45516d4e3e639126f34
SickHekker <Robindehek@gmail.com>
2018-01-10 14:35:03 +0100

    Update toonlog.py

--- toonlog.py
+++ toonlog.py
@@ -2,36 +2,37 @@ from Toon import Toon
 from datetime import datetime
 from influxdb import InfluxDBClient
 from influxdb.client import InfluxDBClientError
+import time
+import sys
+
 
 toonusername = "YOURTOONUSERNAME"
 toonpassword = "YOURTOONPASSWORD"
 
-toon = Toon(toonusername,toonpassword)
+toon = Toon(toonusername, toonpassword)
 
 client = InfluxDBClient('address', port, 'user', 'password', 'database')
 
+
 def save_data(sensor):
-	toon.login()
-    	current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    	thermostat = toon.get_thermostat_info()
-	temperature = temp = float(thermostat["currentTemp"]) / 100
-	toon.logout()
+    toon.login()
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    thermostat = toon.get_thermostat_info()
+    temperature = temp = float(thermostat["currentTemp"]) / 100
+    power = toon.get_power_usage()
+    toon.logout()
     if temperature is not None:
-        print('Sensor={0}  Temp={1:0.1f}*C'.format(sensor, temperature))
-        json_body = [
-            {
-                "measurement": "{0}_temperature".format(sensor),
-                "tags": {
-                    "celsius": "temperature"
-                },
-                "timestamp": current_time,
-                "fields": {
-                    "value": float("{0:0.1f}".format(temperature))
+            json_body = [
+                {
+                    "measurement": "toon",
+                    "timestamp": current_time,
+                    "fields": {
+                        "temperature": float("{0:0.1f}".format(temperature)),
+                        "power": power["value"]
+                    }
                 }
-            }]
-        # print(json_body)
-        client.write_points(json_body)
-		
-		
+            ]
+            # print(json_body) #for debug, not enabled
+            client.write_points(json_body)
 
 save_data('toon')
